8a91f570b3867d10356868be270fea35fbd7691b
Mircy <mircea.gancea@gmail.com>
2016-04-24 10:42:08 +0100

    Added files via upload

---
+++ horticulture.php
@@ -0,0 +1,88 @@
+<?php 
+include "includes/header.php";
+?>
+		<main id="content" role="main">
+			<section id="hortcontent">
+				<header>
+					<h2>CAN HORTICULTURE FEED THE WORLD?</h2>
+				</header>
+				<article id="hortfirst">
+					<div id="flowerdiv">
+						<img src="images/flowerfield.jpg" alt="Flowers field" width="335">
+					</div>
+					<div id="hortfirsttext">
+						<p>First of all, let us decipher the name horticulture into simple English:</p>
+						<p>'hortus' = garden.</p>
+						<p>'cultus' = cultivation.</p>
+						<p>'horticulture' = garden cultivation.</p> 
+						<p>'horticulturist' = gardener.</p> 
+						<p>Yes, it's that simple. Thus my question is, can gardening feed the world? I believe so, and with this presentation I aim to convince others. First of all, the antagonist: Can agriculture feed the world? No. The blunt fact that people, despite our advanced modern technology, are still starving and many agricultural areas in tropical zones are turning into deserts should be proof enough of that.</p>
+					</div>
+					<div id="hortsectext">
+						<p>Horticulture on the other hand deals with a diversity of plants, not only to grow food, but also for medicinal and practical uses: Cloth, cord, dye, isolation, glue, flammable oils, et cetera... A wisely designed garden does not only provide food and medicine, but is also able to sustain us and regenerate itself, without the need of fossil fuels, chemicals nor water. Having studied the teachings of Bill Mollison, Sepp Holzer and many others, horticulture seems to outsmart agriculture in every way except for the sheer force of massproduction. So can horticulture really feed the world?  I believe the answer lies not in HOW we grow, but in WHAT we grow.</p>
+					</div>
+				</article>
+				<article id="hortsecond">
+					<p>I have read anthropological reports claiming that mankind globally and throughout history have used up to 80.000 different plants as sources of food. According to the Plants For A Future database there are still around 20.000 documented edible plants around us, according to wikipedia another 30.000 undocumented. Yet 20 of these now supply for 90% of modern food production: Staple foods like corn, rice, potato, wheat, cassava et cetera. If these numbers are correct we have gone from a diversity of 80.000 edible plants to 20 in just a few hundred years. And these 20 plants are often grown in places where they do not even belong. They need to be grown by force and replanted every year, further stressing and depleting the soil.</p>
+					<p>All of these plants around, providing biodiversity for nature and health for us, and we are using less than a fraction of a percent of the natural resources surrounding us for commercial food production, pretending it’s all normal, while people are starving. Such is the madness of modern agriculture.</p>
+					<p>In my own research I have found more than 300 documented edible and medicinal plants that grow wild in the Aegean region. Many I have located growing as weeds in my garden, others by the roadside or in the forest, and what I have not managed to track down in nature I am introducing the seeds.</p>
+					<p>So, do you really think it is that complicated to set up selfsustainable gardens if we could just expand our horizon beyond 20 hard-to-grow hybrids, and replace them with 300 endemic edibles? It is a simple matter of knowledge and initiative. Personally I could live off my little garden just after one year of experimenting, combining weeds with what I have myself introduced. I still buy food from the market, because let's face it, just like cough medicine, what's good for us sometimes just don't taste that great, and in the long run leafy greens and wildflowers can get sort of boring... But it is doable. Again, this is after one year of research and experimenting! One more year and I might even add a little bit of taste in there...</p>
+					<p>According to Professor Toby Hemenway foragers need 3 hours to gather a week’s worth of food. Farmers need 2-3 days for their food, plus more time for the rent, etc. Still think it's impossible? </p>
+					<blockquote> 
+                            <header>
+                                <cite>Again I quote Hemenway:</cite>
+                            </header>
+							<p>&quot;Agriculture is grounded in fear and insecurity. It is a mindset of scarcity. For domesticated people, wild people are the scariest thing. Wild people don’t need anything from a government. Instead of knowing that nature is enough to provide, nature became the enemy. We are told we don’t belong in the wilderness. A civilization based on agriculture: makes nature the enemy; destroys ecosystems; is based in scarcity; is more work; fosters hierarchy. A civilization disconnected from nature goes insane! Yet when we look at nature, we see abundance, not scarcity. Permaculture is a new horticultural society. Food from a garden, not a farm, small scale, mixed crops, encouraging succession. With permaculture, the ecosystems still function. Permaculture can save humanity and the Earth, but not civilization.&quot;</p>
+                    </blockquote>
+				</article>	
+				<article id="hortthird">
+						<p>Right then, it all seems to boil down to a huge conspiracy theory where we are unconsciously still living under feudalistic rule and food is just another control tool. I will neither deny nor validate that, merely acknowledge the crisis and try to promote a practical solution. But before we get to solutions and revolutions, let's review where we come from, how we lost our instincts, and why the whole world seems such a big mess at the moment.</p>
+						<p class="bold">Here are three theories, all miserably flawed yet equally plausible:</p>
+						<div id="test1" class="test">
+							<img src="images/humanevolution.jpg" alt="Human Evolution" width="320" height="100">
+							<p class="theories">1. The discipline we call science, hitherto the most rational mindset of our young civilisation, pokes its' index finger towards Darwinism; The theory of evolution claims that ever since we combined the forces of the brain and the thumb so as to be able to handle tools we gradually lost our natural instincts and forgot how to feed from the land like our ape ancestors still do.</p>
+						</div>
+						<div class="test">
+							<img src="images/rel1.jpg" alt="American Indian" width="150" height="100">
+							<p class="theories">2. Should you ask a shaman in the rainforest he would probably tell you that we are all spiritual beings locked up in human bodies, and the whole western civilisation has been blinded by an evil spell, a veil over our eyes that has cut us off from our roots and wiped out or natural instincts in order to enslave us, and that the true knowledge on food and medicine comes from communication with spirits.</p>
+						</div>
+						<div class="test">
+							<img src="images/rel2.jpg" alt="Adam and Eve" width="150" height="100">
+							<p class="theories">3. And if we look towards younger monotheistic traditions like Christianity; The Bible says we were cast out of the garden of Eden as a punishment for eating the apple of wisdom, which replaced our natural connection to the soil with egoistic intellect, separating us from nature, henceforth having to force food from the soil by the sweat of our brow.</p>
+						</div>
+						<p id="hortclear">No matter which of these viewpoints you find the least laughable, fact remains that we are the only mammal on the planet who needs clothes and houses, have no instinct on what is our natural diet, and have the peculiar habit of heating food before consumption. Well, it was not always so...</p>
+				</article>
+					
+				<article id="hortforth">
+					
+					<p>Prior to our own selfdestructive civilisation whose aim is to control nature, there are examples of other societies who were actually able to co-exist with nature. They might not have been more technologically advanced, but their knowledge on their surroundings, and how to utilize natural resources without depleting them, were far superior to ours. Hemenway even suggests they enrichened the landscape, as they travelled with the seasons they gathered the seeds of what they found wild and replanted more edible trees, bushes and cereals et cetera. </p>
+					<p>There were no staple foods. Feeding people without depleting the land was as much about food diversity as bio diversity, much in contradiction to modern agriculture. To these semi-nomadic tribes, nature itself was a huge garden abundant with food. Many native American societies are examples of cultures with superior knowledge on their surroundings. Unfortunately they were conquered by a technology-wise far more superior culture: The evil Europeans.</p>
+					<p>But there we have it; A so called primitive culture far more capable of sustaining itself as well as its' surrounding. A less technologically advanced culture had less room for specialisation. There were no webdesigners, no supply managers, no busdrivers, no factoryworkers, hardly any specialisation which our modern society is based upon. So everybody knew about nature, what was edible, and consequently passed that knowledge along to new generations. Every individual knew about edible nuts, berries, fruits and roots, as well as where and when to find them. And because of the abundance and diversity they never depleted any one resource but would actually gather the seeds and spread them, producing more!</p>
+					<p>Now take a good look at modern agriculture: Feeding the world with 20 plants and fossil fuels is poisoning everything, not only making us fat and stupid and sick, but also destroying the natural resources around us. People are still starving not because of lack of food, but because of greed, insanity and ignorance.</p>
+					<p>So how do we fix that?</p>
+					<p>People who put their faith in science are hoping for yet another miracle of science to replace oil. People who ground their beliefs in the shamanistic traditions pray that the warriors of the rainbow will come along to save the day and bring about a new era of enlightenment. And the monotheistic traditions either present gloomy tales of judgement day or the reincarnation of their favourite prophets. Perhaps they are all right, perhaps none, but why idle about and wait for wonders when the tools to create Utopia are hidden in plain sight: Horticulture!</p>
+				</article>
+				<article id="hortfifth">
+					<img src="images/primitive.jpg" alt="Primitive man" width="268" height="250">
+					<p>If we were to take more primitive societies as rolemodels and start identifying what grows wild around us and can be utilized, to plant more and nurture what works, we could create a huge diversity of edible plants in very small areas. Plant families like amaranthus, trifolium, plantago, rumex, chenopodium, brassica & lamium are common invasive weeds all over the world, way healthier than processed food stuff, and those are just the top of the iceberg...</p>
+					<p>Pine is edible, birch can be tapped for water and has edible leaves, many thistles and cacti are edible and the later is a natural air- and water-purifier... The wild ancestors of cabbage and chard are endemic in the Mediterranean, both perennials that will come back year after year... Willow herb, stinging nettle, cow parsley, couch grass and ground elder are invasive in colder climates like Scandinavia... Purslane, burdock, asparagus, cardoon, borage and chicory are supernutritious and put out quite a bulk of food...</p>
+				</article>
+				<article>
+					<p>The wild forms of onion, carrot and sallad are easily introduced in many climates and way hardier than the common hybrids... Sweet potato is actually a perennial climber with edible foilage as well as the tubers... There are many perennial beans and peas that will come back year after year once introduced... Many grasses are edible and way healthier than processed foods... If you pass by a meadow that has not been touched in years there is a great chance that at least half of what grows in there are medicinal flowers like poppy, mallow, daisy, dandelion, chamomile, cornflower, chrysanthemum... The list goes on and on, and we are still at the top of the iceberg!</p>
+					<p>Knowledge and supply are already accessible; Tons of books and websites have been dedicated to permaculture, wild edibles and medicinal plantlore, and seed suppliers are selling a huge diversity of seeds, of which many are not only edible and medicinal, but will spread like weed faster than we can consume them.</p>
+					<p>Many regions in the world seem to be beyond the point of no return, but many great minds are working on how to regrow the deserts: Masanobu Fukuoka suggests to fill bomb planes with seeds and bomb the edges of our deserts, see what grows and plant more. The hardiest plants will take root and roots retain water, and soil that retains water can be cultivated. Fukuoka also points out the simple fact that where resources are abundant there is nothing to fight over.</p>
+					<blockquote>    
+                            <header>
+                                <cite>R. Buckminster Fuller further promotes this idea, saying</cite>
+                            </header>
+							<p>&quot;It is now highly feasible to take care of everybody on Earth at a higher standard of living than any have ever known. It no longer has to be you or me. Selfishness is unnecessary. War is obsolete. It is a matter of converting the high technology from weaponry to livingry&quot;</p>
+                    </blockquote>
+					<p>As for the rich soil of the Aegean and Mediterranean regions it is but a matter of education and initiative, hence my reason for creating this website.</p>
+					<p>For I believe the future is not by the horizon, but at our feet!</p>
+				</article>
+				
+			</section>
+		</main>
+<?php 
+include "includes/footer.php";
+?>	
---
+++ index.php
@@ -0,0 +1,41 @@
+<?php 
+include "includes/header.php";
+?>
+		<main id="content" role="main">
+			<section id="homecontent">
+				<header>
+					<blockquote>
+                            <p>&quot;It is now highly feasible to take care of everybody on Earth at a higher standard of living than any have ever known. It no longer has to be you or me. Selfishness is unnecessary. War is obsolete. It is a matter of converting the high technology from weaponry to livingry.&quot;</p>
+                            <footer>
+                                <cite>- R. Buckminster Fuller</cite>
+                            </footer>
+                    </blockquote>
+				</header>
+				<div>
+					<p>Our guts are long - like that of herbivores, which are designed to digest vegetation. The guts of carnivores are short to enable them to expel decomposing meat residues quickly. Meat is full of toxins from the animal at the time of death, and these toxins can be absorbed into the blood, contributing to all sorts of disorders, including headaches, arthritis, cancer, diabetes and heart disease. Because we do not have a digestive system designed to cope with meat, our health will suffer if we eat it.</p>
+					<p>This is one theory. It is not bullet proof. There are many other theories, in fact there's an abundance of theories floating about... And so the debate on what is 'natural' food for our own rather unnatural species (seemingly void of instinct and unable to fit into any natural biotope of the planet) rages on, all whilst famine and diseases and cancer haunt our species at an accelerated rate. In the search for solid evidence of what is supposedly good for human health and longevity proper evidence remains lucid and theoreticall, where as the antagonists are quite obvious:</p>
+					<p class="boldtext">What is bad:</p>
+					<ul>
+						<li>Since 1940 we have released more than 140 000 substances into the atmosphere. 3 % of these have been researched. These affect the air, the water, the soil, plantlife, what we eat, what we drink, and us.</li>
+						<li>Majority of the modern world depend on money and grocery stores to sustain themselves: In effect this means our species has domesticated a few wild plants and made them dull through mono-cropping, hybridization, pesticides and gene-manipulation, only to ship them half a world away - soaked in conservational chemicals and wrapped in plastic, containing no more than a ghost of all the nutrients you would find in their wild ancestors.</li>
+						<li>As our immune systems inevitably break down from lousy food we develop diseases and we need medicines, another plant source is gathered, concentrated in the form of pills or liquid, sterilized, wrapped in plastic, labelled, and shipped half over the world. Basically our whole species herded into neat little rows running back and forth between the grocery stores and the hospitals. This is a short term solution which promotes population control, rather than long term health.</li>
+						<li>With a rapidly growing global population and an increasingly unpredictable climate, food security has become a serious concern. There are over 20.000 species of edible plants known in the world, yet fewer than 20 species now provide 90% of our food. Yet as mono-cropping, pesticides and biochemistry backfires over and over again, even more resources are pushed into the same old machinery, wreaking havoc upon our natural biotopes. Rather than establishing sustainable food production systems we are escalating into a fullscale war on mother nature. The outcome of such a strategy is as predictable as it is insane as it is fatal.</li>
+					</ul>
+					<p>Fortunately the solution is simple: Any halfwit of a decent intellect knows that the key  to dealing with problems is not through solutions, but to avoid creating problems in the first place: I.e. prevention! - Imagine a fraction of all the money invested in weapons technology, genetic seed research, Hollywood, bad reality shows, and the fashion industry - to the research and public education on the other 19.980 edible plants surrounding us: Free food and medicine for everyone.</p>
+					<p>But since the control apparatus obviously figures Utopia a bad monetary investment it is up to us little people to gather and share knowledge, inspire and create, and hopefully one day redirect our task-incompatible leaders towards a new task; To convince them that the outmost aim of humanity need not be to control the human being, but to set her free!</p>
+					<p>This is my contribution.</p>
+				</div>
+				<footer>
+					<p class="boldtext">Thus, the aim of this site is:</p>
+					<ol>
+						<li>To present a list of wild edibles in the region I live, as well as describing their edible and medicinal uses. </li>
+						<li>To create a practical blueprint on how to set up a small scale garden using the most common edible- and medicinal- plants that are either perennial or invasive, to allow for anyone with access to a small patch of land to approach self sustainability at the expense of little or no maintenance.</li>
+						<li> To reduce the distance food and medicine has to travel (from that of another continent to your backyard), hopefully decreasing the abuse of fossil fuels and improve population health in the process.</li>
+						<li>Make the world a slightly less horrible place.</li>
+					</ol>
+				</footer>
+			</section>
+		</main>
+<?php 
+include "includes/footer.php";
+?>	
