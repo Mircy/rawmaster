52c9800fee788ef4a1cda256bf3bf44140fee632
Mircy <mircea.gancea@gmail.com>
2017-09-19 22:25:13 +0100

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Setting information that will be used through the code
-//$config['db_host'] = 'localhost';
-//$config['db_user'] = 'coco';
-//$config['db_pass'] = 'coco2016';
-//$config['db_name'] = 'cocodb';
-$config['language'] = 'en';
-$config['css'] = 'css/coco.css';
-?>
